5d11219c6288034cdcd81ec4b8d1a74076b8b24e
Ryan Dawes <dev@dawes.cc>
2025-03-14 16:26:58 -0400

    Swapped Iowa St for Tennessee, Big12 for SEC + team '66' to '2633' + conference '8' to '23'

--- app/espn.tsx
+++ app/espn.tsx
@@ -1,270 +1,247 @@
 export type TeamBasicInfo = {
-  id: string;
-  displayName: string;
-  logo?: string;
-  color?: string;
+	id: string;
+	displayName: string;
+	logo?: string;
+	color?: string;
 };
 
 type GameData = {
-  id: string;
-  date: string;
-  name: string;
-  teamId: string;
-  logo: string;
-  color: string;
-  rank: number;
-  selectedTeamRank: number;
-  homeScore?: number;
-  awayScore?: number;
-  winner?: boolean;
+	id: string;
+	date: string;
+	name: string;
+	teamId: string;
+	logo: string;
+	color: string;
+	rank: number;
+	selectedTeamRank: number;
+	homeScore?: number;
+	awayScore?: number;
+	winner?: boolean;
 };
 
 type TeamData = {
-  id: string;
-  name: string;
-  logo: string;
-  color: string;
-  record: string;
-  standing: string;
-  games: GameData[];
+	id: string;
+	name: string;
+	logo: string;
+	color: string;
+	record: string;
+	standing: string;
+	games: GameData[];
 };
 
 type CompetitorData = {
-  id: string;
-  homeAway: string;
-  team: TeamBasicInfo;
-  score?: {
-    value: number;
-    displayValue: string;
-  };
-  winner?: boolean;
-  records?: { summary: string }[];
-  curatedRank: { current: number };
+	id: string;
+	homeAway: string;
+	team: TeamBasicInfo;
+	score?: {
+		value: number;
+		displayValue: string;
+	};
+	winner?: boolean;
+	records?: { summary: string }[];
+	curatedRank: { current: number };
 };
 
 type ConferenceRankingEntry = {
-  name: string;
-  teamId: string;
-  logo: string;
-  color: string;
-  conferenceWinLoss: string;
-  gamesBack: string;
-  overallWinLoss: string;
+	name: string;
+	teamId: string;
+	logo: string;
+	color: string;
+	conferenceWinLoss: string;
+	gamesBack: string;
+	overallWinLoss: string;
 };
 
-const DARK_LOGO_TEAMS = [
-  'Iowa Hawkeyes',
-  'Long Beach State Beach',
-  'Cincinnati Bearcats'
-];
-const DEFAULT_LOGO =
-  'https://a.espncdn.com/i/teamlogos/default-team-logo-500.png';
+const DARK_LOGO_TEAMS = ["Iowa Hawkeyes", "Long Beach State Beach", "Cincinnati Bearcats"];
+const DEFAULT_LOGO = "https://a.espncdn.com/i/teamlogos/default-team-logo-500.png";
 
 function getTeamColor(teamName: string): string {
-  return DARK_LOGO_TEAMS.includes(teamName) ? '000000' : 'N/A';
+	return DARK_LOGO_TEAMS.includes(teamName) ? "000000" : "N/A";
 }
 
 function getStat(stats: any[], name: string): string {
-  return stats.find((stat: any) => stat.name === name)?.displayValue ?? '';
+	return stats.find((stat: any) => stat.name === name)?.displayValue ?? "";
 }
 
 function formatDateTime(date: Date) {
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
+	const today = new Date();
+	const tomorrow = new Date(today);
+	tomorrow.setDate(tomorrow.getDate() + 1);
 
-  const isToday = date.toDateString() === today.toDateString();
-  const isTomorrow = date.toDateString() === tomorrow.toDateString();
+	const isToday = date.toDateString() === today.toDateString();
+	const isTomorrow = date.toDateString() === tomorrow.toDateString();
 
-  const formattedTime = date.toLocaleTimeString('en-US', {
-    hour: 'numeric',
-    minute: '2-digit',
-    timeZone: 'America/Chicago'
-  });
+	const formattedTime = date.toLocaleTimeString("en-US", {
+		hour: "numeric",
+		minute: "2-digit",
+		timeZone: "America/Chicago",
+	});
 
-  let formattedDate;
-  if (isToday) {
-    formattedDate = 'Today';
-  } else if (isTomorrow) {
-    formattedDate = 'Tomorrow';
-  } else {
-    formattedDate = date.toLocaleDateString('en-US', {
-      month: 'numeric',
-      day: 'numeric'
-    });
-  }
+	let formattedDate;
+	if (isToday) {
+		formattedDate = "Today";
+	} else if (isTomorrow) {
+		formattedDate = "Tomorrow";
+	} else {
+		formattedDate = date.toLocaleDateString("en-US", {
+			month: "numeric",
+			day: "numeric",
+		});
+	}
 
-  return `${formattedDate} - ${formattedTime}`;
+	return `${formattedDate} - ${formattedTime}`;
 }
 
 export async function getTeamData(teamId: string): Promise<TeamData> {
-  if (teamId.includes('teamId')) {
-    return {
-      id: teamId,
-      name: '',
-      logo: '',
-      color: '',
-      record: '',
-      standing: '',
-      games: []
-    };
-  }
+	if (teamId.includes("teamId")) {
+		return {
+			id: teamId,
+			name: "",
+			logo: "",
+			color: "",
+			record: "",
+			standing: "",
+			games: [],
+		};
+	}
 
-  const res = await fetch(
-    `https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamId}/schedule`
-  );
+	const res = await fetch(`https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamId}/schedule`);
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch team data: ${res.statusText}`);
-  }
+	if (!res.ok) {
+		throw new Error(`Failed to fetch team data: ${res.statusText}`);
+	}
 
-  const data = await res.json();
+	const data = await res.json();
 
-  const games: GameData[] = data.events.map((event: any) => {
-    const competitors = event.competitions[0].competitors;
-    const favoriteTeam = competitors.find((team: any) => team.id === teamId);
-    const otherTeam = competitors.find((team: any) => team.id !== teamId);
+	const games: GameData[] = data.events.map((event: any) => {
+		const competitors = event.competitions[0].competitors;
+		const favoriteTeam = competitors.find((team: any) => team.id === teamId);
+		const otherTeam = competitors.find((team: any) => team.id !== teamId);
 
-    if (!favoriteTeam || !otherTeam) {
-      throw new Error(
-        'Expected to find both the favorite team and an opposing team in the event competitors'
-      );
-    }
+		if (!favoriteTeam || !otherTeam) {
+			throw new Error("Expected to find both the favorite team and an opposing team in the event competitors");
+		}
 
-    const color = getTeamColor(otherTeam.team.displayName);
-    const logo = otherTeam.team.logos?.[0]?.href ?? DEFAULT_LOGO;
+		const color = getTeamColor(otherTeam.team.displayName);
+		const logo = otherTeam.team.logos?.[0]?.href ?? DEFAULT_LOGO;
 
-    const date = new Date(event.competitions[0].date);
+		const date = new Date(event.competitions[0].date);
 
-    return {
-      id: event.competitions[0].id,
-      date: formatDateTime(date),
-      name: otherTeam.team.displayName,
-      teamId: otherTeam.team.id,
-      rank: otherTeam.curatedRank.current,
-      selectedTeamRank: favoriteTeam.curatedRank.current,
-      logo,
-      color,
-      homeScore: favoriteTeam.score?.value,
-      awayScore: otherTeam.score?.value,
-      winner: favoriteTeam.winner
-    };
-  });
+		return {
+			id: event.competitions[0].id,
+			date: formatDateTime(date),
+			name: otherTeam.team.displayName,
+			teamId: otherTeam.team.id,
+			rank: otherTeam.curatedRank.current,
+			selectedTeamRank: favoriteTeam.curatedRank.current,
+			logo,
+			color,
+			homeScore: favoriteTeam.score?.value,
+			awayScore: otherTeam.score?.value,
+			winner: favoriteTeam.winner,
+		};
+	});
 
-  return {
-    id: teamId,
-    name: data.team.displayName,
-    logo: data.team.logo,
-    color: data.team.color,
-    record: data.team.recordSummary,
-    standing: data.team.standingSummary,
-    games
-  };
+	return {
+		id: teamId,
+		name: data.team.displayName,
+		logo: data.team.logo,
+		color: data.team.color,
+		record: data.team.recordSummary,
+		standing: data.team.standingSummary,
+		games,
+	};
 }
 
 export async function getAllTeamIds(): Promise<TeamBasicInfo[]> {
-  const pagePromises = Array.from({ length: 8 }, (_, i) =>
-    fetch(
-      `https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams?page=${i + 1}`
-    ).then((res) => {
-      if (!res.ok) {
-        throw new Error(`Failed to fetch team IDs: ${res.statusText}`);
-      }
-      return res.json();
-    })
-  );
+	const pagePromises = Array.from({ length: 8 }, (_, i) =>
+		fetch(`https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams?page=${i + 1}`).then((res) => {
+			if (!res.ok) {
+				throw new Error(`Failed to fetch team IDs: ${res.statusText}`);
+			}
+			return res.json();
+		})
+	);
 
-  const dataArray = await Promise.all(pagePromises);
-  const teams: TeamBasicInfo[] = dataArray.flatMap((data) =>
-    data.sports[0].leagues[0].teams.map((team: any) => team.team)
-  );
+	const dataArray = await Promise.all(pagePromises);
+	const teams: TeamBasicInfo[] = dataArray.flatMap((data) => data.sports[0].leagues[0].teams.map((team: any) => team.team));
 
-  return teams.sort((a, b) => a.displayName.localeCompare(b.displayName));
+	return teams.sort((a, b) => a.displayName.localeCompare(b.displayName));
 }
 
 export async function getTodaySchedule() {
-  const res = await fetch(
-    'https://site.web.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard'
-  );
+	const res = await fetch("https://site.web.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard");
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch today's schedule: ${res.statusText}`);
-  }
+	if (!res.ok) {
+		throw new Error(`Failed to fetch today's schedule: ${res.statusText}`);
+	}
 
-  const data = await res.json();
+	const data = await res.json();
 
-  const games = data.events.map((event: any) => {
-    const [homeTeam, awayTeam] = event.competitions[0].competitors;
+	const games = data.events.map((event: any) => {
+		const [homeTeam, awayTeam] = event.competitions[0].competitors;
 
-    if (!homeTeam || !awayTeam) {
-      throw new Error(
-        'Expected to find both home and away teams in the event competitors'
-      );
-    }
+		if (!homeTeam || !awayTeam) {
+			throw new Error("Expected to find both home and away teams in the event competitors");
+		}
 
-    const date = new Date(event.date);
+		const date = new Date(event.date);
 
-    return {
-      status: event.competitions[0].status.type.shortDetail,
-      date: formatDateTime(date),
-      homeTeam: formatTeamData(homeTeam),
-      awayTeam: formatTeamData(awayTeam)
-    };
-  });
+		return {
+			status: event.competitions[0].status.type.shortDetail,
+			date: formatDateTime(date),
+			homeTeam: formatTeamData(homeTeam),
+			awayTeam: formatTeamData(awayTeam),
+		};
+	});
 
-  return {
-    games
-  };
+	return {
+		games,
+	};
 }
 
 function formatTeamData(teamData: CompetitorData) {
-  return {
-    name: teamData.team.displayName,
-    teamId: teamData.team.id,
-    rank: teamData.curatedRank.current,
-    logo: teamData.team.logo ?? DEFAULT_LOGO,
-    color: getTeamColor(teamData.team.displayName),
-    score: teamData.score,
-    winner: teamData.winner,
-    record: teamData.records
-      ? `(${teamData.records[0].summary}, ${teamData.records[3]?.summary ?? 'N/A'})`
-      : 'N/A'
-  };
+	return {
+		name: teamData.team.displayName,
+		teamId: teamData.team.id,
+		rank: teamData.curatedRank.current,
+		logo: teamData.team.logo ?? DEFAULT_LOGO,
+		color: getTeamColor(teamData.team.displayName),
+		score: teamData.score,
+		winner: teamData.winner,
+		record: teamData.records ? `(${teamData.records[0].summary}, ${teamData.records[3]?.summary ?? "N/A"})` : "N/A",
+	};
 }
 
-export async function getConferenceRankings(): Promise<
-  ConferenceRankingEntry[]
-> {
-  const res = await fetch(
-    'https://site.web.api.espn.com/apis/v2/sports/basketball/mens-college-basketball/standings?region=us&lang=en&contentorigin=espn&group=8&season=2025'
-  );
+export async function getConferenceRankings(): Promise<ConferenceRankingEntry[]> {
+	const res = await fetch(
+		"https://site.web.api.espn.com/apis/v2/sports/basketball/mens-college-basketball/standings?region=us&lang=en&contentorigin=espn&group=23&season=2025"
+	);
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch conference rankings: ${res.statusText}`);
-  }
+	if (!res.ok) {
+		throw new Error(`Failed to fetch conference rankings: ${res.statusText}`);
+	}
 
-  const data = await res.json();
+	const data = await res.json();
 
-  let teamsData = data.standings.entries.map((entry: any) => {
-    const { team, stats } = entry;
+	let teamsData = data.standings.entries.map((entry: any) => {
+		const { team, stats } = entry;
 
-    return {
-      name: team.displayName,
-      teamId: team.id,
-      logo: team.logos[0]?.href ?? DEFAULT_LOGO,
-      color: getTeamColor(team.displayName),
-      conferenceWinLoss: getStat(stats, 'vs. Conf.'),
-      gamesBack: getStat(stats, 'gamesBehind'),
-      overallWinLoss: `${getStat(stats, 'wins')}-${getStat(stats, 'losses')}`
-    };
-  });
+		return {
+			name: team.displayName,
+			teamId: team.id,
+			logo: team.logos[0]?.href ?? DEFAULT_LOGO,
+			color: getTeamColor(team.displayName),
+			conferenceWinLoss: getStat(stats, "vs. Conf."),
+			gamesBack: getStat(stats, "gamesBehind"),
+			overallWinLoss: `${getStat(stats, "wins")}-${getStat(stats, "losses")}`,
+		};
+	});
 
-  return teamsData.sort(
-    (a: ConferenceRankingEntry, b: ConferenceRankingEntry) => {
-      if (a.gamesBack === '-' && b.gamesBack !== '-') return -1;
-      if (a.gamesBack !== '-' && b.gamesBack === '-') return 1;
-      if (a.gamesBack === '-' && b.gamesBack === '-') return 0;
-      return parseFloat(a.gamesBack) - parseFloat(b.gamesBack);
-    }
-  );
+	return teamsData.sort((a: ConferenceRankingEntry, b: ConferenceRankingEntry) => {
+		if (a.gamesBack === "-" && b.gamesBack !== "-") return -1;
+		if (a.gamesBack !== "-" && b.gamesBack === "-") return 1;
+		if (a.gamesBack === "-" && b.gamesBack === "-") return 0;
+		return parseFloat(a.gamesBack) - parseFloat(b.gamesBack);
+	});
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,142 +1,116 @@
-import clsx from 'clsx';
-import Link from 'next/link';
-import Image from 'next/image';
-import { getAllTeamIds, getTeamData } from 'app/espn';
-import { unstable_cacheLife as cacheLife } from 'next/cache';
-import TeamSelect from './[teamId]/select';
-import ConferencePage from './conference/page';
-import ScoresPage from './scores/page';
+import clsx from "clsx";
+import Link from "next/link";
+import Image from "next/image";
+import { getAllTeamIds, getTeamData } from "app/espn";
+import { unstable_cacheLife as cacheLife } from "next/cache";
+import TeamSelect from "./[teamId]/select";
+import ConferencePage from "./conference/page";
+import ScoresPage from "./scores/page";
 
-function Row({
-  awayScore,
-  color,
-  date,
-  homeScore,
-  index,
-  isLast,
-  logo,
-  name,
-  rank,
-  teamId,
-  winner
-}: any) {
-  return (
-    <div
-      className={clsx(
-        'flex flex-col min-[450px]:flex-row justify-between px-0 py-2',
-        { 'border-b border-gray-200 dark:border-gray-800': !isLast }
-      )}
-    >
-      <div className="flex">
-        <Image
-          src={logo}
-          alt={name}
-          priority={index < 10}
-          width={20}
-          height={20}
-          className={clsx('h-5 w-5', {
-            'dark:invert': color === '000000'
-          })}
-        />
-        <Link href={`/${teamId}`} className="font-semibold ml-4">
-          {rank !== 99 ? (
-            <span className="text-sm uppercase font-normal text-gray-500 mr-2">
-              {rank}
-            </span>
-          ) : null}
-          {name}
-        </Link>
-      </div>
-      <div className="flex flex-row-reverse justify-end min-[450px]:flex-row">
-        {homeScore ? (
-          <p className="text-gray-700 dark:text-gray-300 tabular-nums">{`${homeScore}-${awayScore}`}</p>
-        ) : null}
-        {homeScore ? (
-          winner ? (
-            <p className="font-semibold text-green-700 dark:text-green-500 ml-0 min-[450px]:ml-2 w-5 mr-4 min-[450px]:mr-0 text-center">
-              W
-            </p>
-          ) : (
-            <p className="font-semibold text-red-700 dark:text-red-500 ml-0 min-[450px]:ml-2 w-5 mr-4 min-[450px]:mr-0 text-center">
-              L
-            </p>
-          )
-        ) : homeScore === 0 ? (
-          <p className="font-semibold text-gray-500 ml-0 min-[450px]:ml-2 w-5 mr-4 min-[450px]:mr-0 text-center">
-            —
-          </p>
-        ) : (
-          <p className="text-gray-700 dark:text-gray-300">{date}</p>
-        )}
-      </div>
-    </div>
-  );
+function Row({ awayScore, color, date, homeScore, index, isLast, logo, name, rank, teamId, winner }: any) {
+	return (
+		<div className={clsx("flex flex-col min-[450px]:flex-row justify-between px-0 py-2", { "border-b border-gray-200 dark:border-gray-800": !isLast })}>
+			<div className='flex'>
+				<Image
+					src={logo}
+					alt={name}
+					priority={index < 10}
+					width={20}
+					height={20}
+					className={clsx("h-5 w-5", {
+						"dark:invert": color === "000000",
+					})}
+				/>
+				<Link
+					href={`/${teamId}`}
+					className='font-semibold ml-4'>
+					{rank !== 99 ? <span className='text-sm uppercase font-normal text-gray-500 mr-2'>{rank}</span> : null}
+					{name}
+				</Link>
+			</div>
+			<div className='flex flex-row-reverse justify-end min-[450px]:flex-row'>
+				{homeScore ? <p className='text-gray-700 dark:text-gray-300 tabular-nums'>{`${homeScore}-${awayScore}`}</p> : null}
+				{homeScore ? (
+					winner ? (
+						<p className='font-semibold text-green-700 dark:text-green-500 ml-0 min-[450px]:ml-2 w-5 mr-4 min-[450px]:mr-0 text-center'>W</p>
+					) : (
+						<p className='font-semibold text-red-700 dark:text-red-500 ml-0 min-[450px]:ml-2 w-5 mr-4 min-[450px]:mr-0 text-center'>L</p>
+					)
+				) : homeScore === 0 ? (
+					<p className='font-semibold text-gray-500 ml-0 min-[450px]:ml-2 w-5 mr-4 min-[450px]:mr-0 text-center'>—</p>
+				) : (
+					<p className='text-gray-700 dark:text-gray-300'>{date}</p>
+				)}
+			</div>
+		</div>
+	);
 }
 
 async function Schedule() {
-  'use cache';
-  cacheLife('hours');
+	"use cache";
+	cacheLife("hours");
 
-  const [team, allTeams] = await Promise.all([
-    getTeamData('66'),
-    getAllTeamIds()
-  ]);
-  const { name, record, color, standing, games, logo } = team;
-  const nextGame = games.find((game) => game.homeScore === undefined);
+	const [team, allTeams] = await Promise.all([getTeamData("2633"), getAllTeamIds()]);
+	const { name, record, color, standing, games, logo } = team;
+	const nextGame = games.find((game) => game.homeScore === undefined);
 
-  return (
-    <section className="w-full mx-auto p-6">
-      <div className="flex items-center">
-        <Image
-          src={logo}
-          alt="Logo"
-          priority
-          width={24}
-          height={24}
-          className={clsx('h-6 w-6', {
-            'dark:invert': color === '000000'
-          })}
-        />
-        {nextGame?.selectedTeamRank !== 99 ? (
-          <span className="text-sm uppercase font-normal text-gray-500 ml-2">
-            {nextGame?.selectedTeamRank}
-          </span>
-        ) : null}
-        <h1 className="font-semibold text-2xl ml-2">{name}</h1>
-      </div>
-      <h3 className="text-gray-700 dark:text-gray-300 mb-2">{`${record} • ${standing}`}</h3>
-      <TeamSelect allTeams={allTeams} teamId={'66'} />
-      <h2 className="font-semibold text-xl">Schedule</h2>
-      <h3 className="font-semibold text-gray-700 dark:text-gray-300">Next</h3>
-      {nextGame && <Row isLast {...nextGame} />}
-      <h3 className="font-semibold text-gray-700 dark:text-gray-300 mt-4">
-        Full
-      </h3>
-      <div>
-        {games.map((game, index) => (
-          <Row
-            key={game.id}
-            index={index}
-            isLast={index === games.length - 1}
-            {...game}
-          />
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section className='w-full mx-auto p-6'>
+			<div className='flex items-center'>
+				<Image
+					src={logo}
+					alt='Logo'
+					priority
+					width={24}
+					height={24}
+					className={clsx("h-6 w-6", {
+						"dark:invert": color === "000000",
+					})}
+				/>
+				{nextGame?.selectedTeamRank !== 99 ? (
+					<span className='text-sm uppercase font-normal text-gray-500 ml-2'>{nextGame?.selectedTeamRank}</span>
+				) : null}
+				<h1 className='font-semibold text-2xl ml-2'>{name}</h1>
+			</div>
+			<h3 className='text-gray-700 dark:text-gray-300 mb-2'>{`${record} • ${standing}`}</h3>
+			<TeamSelect
+				allTeams={allTeams}
+				teamId={"2633"}
+			/>
+			<h2 className='font-semibold text-xl'>Schedule</h2>
+			<h3 className='font-semibold text-gray-700 dark:text-gray-300'>Next</h3>
+			{nextGame && (
+				<Row
+					isLast
+					{...nextGame}
+				/>
+			)}
+			<h3 className='font-semibold text-gray-700 dark:text-gray-300 mt-4'>Full</h3>
+			<div>
+				{games.map((game, index) => (
+					<Row
+						key={game.id}
+						index={index}
+						isLast={index === games.length - 1}
+						{...game}
+					/>
+				))}
+			</div>
+		</section>
+	);
 }
 
 export default function HomePage() {
-  return (
-    <>
-      <main className="md:hidden">
-        <Schedule />
-      </main>
-      <main className="hidden md:grid md:grid-cols-2 lg:grid-cols-3 divide-x divide-gray-200 dark:divide-gray-800">
-        <Schedule />
-        <ScoresPage />
-        <ConferencePage />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<main className='md:hidden'>
+				<Schedule />
+			</main>
+			<main className='hidden md:grid md:grid-cols-2 lg:grid-cols-3 divide-x divide-gray-200 dark:divide-gray-800'>
+				<Schedule />
+				<ScoresPage />
+				<ConferencePage />
+			</main>
+		</>
+	);
 }
