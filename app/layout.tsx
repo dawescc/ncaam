17160134db40b1de4a7cd30fecd3f8bc3b17df95
Ryan Dawes <dev@dawes.cc>
2025-03-14 16:31:44 -0400

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,38 @@
-import { Suspense } from 'react';
-import './globals.css';
-import { BottomNav } from './navbar';
+import { Suspense } from "react";
+import "./globals.css";
+import { BottomNav } from "./navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: {
-    default: 'NCCAM Scores & Schedules',
-    template: '%s | NCCAM Scores & Schedules',
-  },
-  description: 'Like ESPN, but streamlined.',
+	title: {
+		default: "NCCAM Scores & Schedules",
+		template: "%s | NCCAM Scores & Schedules",
+	},
+	description: "Like ESPN, but streamlined.",
 };
 
 export const viewport = {
-  maximumScale: 1, // Disable auto-zoom on mobile Safari
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
-    { media: '(prefers-color-scheme: dark)', color: '#000000' },
-  ],
+	maximumScale: 1, // Disable auto-zoom on mobile Safari
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "#ffffff" },
+		{ media: "(prefers-color-scheme: dark)", color: "#000000" },
+	],
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className="antialiased">
-      <body className="flex flex-col min-h-screen text-black dark:text-white bg-white dark:bg-black antialiased">
-        <div className="flex-grow overflow-y-scroll h-[calc(100vh_-_80px)] border-b border-gray-200 dark:border-gray-800 pb-16 md:pb-0">
-          {children}
-        </div>
-        <nav className="md:hidden fixed bottom-0 left-0 right-0 h-[80px] bg-white dark:bg-black border-t border-gray-200 dark:border-gray-800">
-          <Suspense fallback={null}>
-            <BottomNav />
-          </Suspense>
-        </nav>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html
+			lang='en'
+			className='antialiased'>
+			<body className='flex flex-col min-h-screen text-black dark:text-white bg-white dark:bg-black antialiased'>
+				<div className='flex-grow overflow-y-scroll h-[calc(100vh_-_80px)] border-b border-gray-200 dark:border-gray-800 pb-16 md:pb-0'>{children}</div>
+				<nav className='md:hidden fixed bottom-0 left-0 right-0 h-[80px] bg-white dark:bg-black border-t border-gray-200 dark:border-gray-800'>
+					<Suspense fallback={null}>
+						<BottomNav />
+					</Suspense>
+				</nav>
+				<Analytics />
+			</body>
+		</html>
+	);
 }
